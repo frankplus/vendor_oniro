c9aa8cdd961f988973df3f6f3a777cdbecac6697
d1124 <13501249800@163.com>
2023-02-03 16:41:34 +0800

    delete ._

--- rpi3/hdf_config/khdf/.hdf_hcs_hex.o.d
+++
@@ -1,5 +0,0 @@
-drivers/hdf/khdf/../../../../vendor/raspberrypi/rpi3/hdf_config/khdf/hdf_hcs_hex.o: \
-  /home/dy/rpi3/ohos/vendor/raspberrypi/rpi3/hdf_config/khdf/hdf_hcs_hex.c \
-  include/linux/kconfig.h include/generated/autoconf.h \
-  include/linux/compiler_types.h include/linux/compiler_attributes.h \
-  include/linux/compiler-clang.h
